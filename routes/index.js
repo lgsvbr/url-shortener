c489b88d0ff1816ae4de408b989128c1e9427ec7
leonardogabrielbr <lgsvbr@gmail.com>
2026-04-29 11:14:55 -0300

    modified

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,18 @@ router.get('/', (req, res, next) => {
   res.render('index', { title: 'Encurtador' });
 });
 
+function generateCode() {
+  let text = '';
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < 5; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  return text;
+}
+
 router.post('/new', (req, res, next) => {
   const url = req.body.url;
-  console.log(url);
-  res.send(url);
+  const code = generateCode();
+  res.send('http://localhost:3000/' + code);
 })
 
 module.exports = router;
