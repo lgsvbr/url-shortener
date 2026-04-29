87e448395c44af1cb11190d6d8f55e9a536571b2
leonardogabrielbr <lgsvbr@gmail.com>
2026-04-28 21:27:26 -0300

    modified:   package.json, routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Encurtador' });
 });
+
+router.post('/new', (req, res, next) => {
+  const url = req.body.url;
+  console.log(url);
+  res.send(url);
+})
 
 module.exports = router;
